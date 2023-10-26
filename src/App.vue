1e8b794c2f45dea2073a0d6ab6a1baedc56efca5
kdjordan <k.dean.jordan@gmail.com>
2023-10-26 16:01:35 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+	import { onMounted } from 'vue';
+	import { gsap } from 'gsap';
+	import TopNavigation from './components/TopNavigation.vue';
+
+	onMounted(() => {
+		gsap.from('.home', { y: -2000, opacity: 0 });
+		gsap.to('.home', {
+			duration: 2,
+			y: 0,
+			opacity: 1,
+			ease: 'power4.out',
+		});
+	});
+</script>
+
+<template>
+	<div class="home bg-blue-500 h-screen flex flex-col items-center">
+		<TopNavigation />
+		<div class="p-32 border-4 border-white">
+			<h1>Home</h1>
+		</div>
+	</div>
+</template>
---
+++ src/components/TopNavigation.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="fixed top-0 left-0 w-full bg-black h-16">
+		<nav class="flex justify-between items-center h-full mx-8">
+			<div class="nav-left">
+        <img src="../assets/logo.svg" width="50" height="50" />
+      </div>
+			<div class="nav-right">
+				<ul class="flex gap-4 text-white">
+					<li>Blog</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</template>
