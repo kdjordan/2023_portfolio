42c44aa8ce8e06f3fd74623b8146ba063dd3f3bc
kdjordan <k.dean.jordan@gmail.com>
2023-10-28 08:20:19 -0700

    font awesome icon support added

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 	import Name from './Name.vue';
-  import { gsap } from 'gsap';
+	import { gsap } from 'gsap';
 	import { onMounted } from 'vue';
+	
+
 
 	onMounted(() => {
 		const tl = gsap.timeline();
-    tl.set('.top-nav', { opacity: 0, borderBottom: 'none' })
-    tl.to('.top-nav', { delay: 2, opacity: 1, duration: 1 });
-    tl.to('.top-nav', { borderBottom: '1px solid #9FF279' });
-  })
+		tl.set('.top-nav', { opacity: 0, borderBottom: 'none' });
+		tl.to('.top-nav', { delay: 2, opacity: 1, duration: 1 });
+		tl.to('.top-nav', { borderBottom: '1px solid #9FF279' });
+	});
 </script>
 
 <template>
-	<div
-		class="top-nav fixed top-0 left-0 w-full h-16 font-oswald"
-	>
+	<div class="top-nav fixed top-0 left-0 w-full h-16 font-oswald">
 		<nav class="flex justify-between items-center h-full mx-[50px]">
 			<div class="nav-left">
 				<Name />
@@ -22,8 +22,9 @@
 			<div class="nav-right">
 				<ul class="flex gap-4 text-highGreen text-md uppercase">
 					<li><router-link to="/"></router-link></li>
-					<li>About</li>
-					<li>Contact</li>
+					<li><font-awesome-icon icon="fa-solid fa-at" /></li>
+					<li><font-awesome-icon icon="fa-brands fa-github" /></li>
+					<li><font-awesome-icon icon="fa-brands fa-linkedin" /></li>
 				</ul>
 			</div>
 		</nav>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,12 @@ import router from './router'
 import './style.css'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).use(router).mount('#app')
+library.add(faGithub, faLinkedin, fas)
+
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
