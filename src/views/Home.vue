1c5052bf36900739165f9c888f2c44abab3aca0d
kdjordan <k.dean.jordan@gmail.com>
2023-11-01 20:29:49 -0700

    about section based

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,85 @@
 <template>
-	<section class="flex flex-col h-screen justify-center items-center pr-[20vw]">
-	<div
-		class="flex flex-col text-stone-300 text-2xl px-4 pb-[25vh] gap-[20px] md:gap-[40px] lg:max-w-[750px] lg:mx-auto"
+	<section
+		class="flex flex-col h-screen justify-center items-center pr-[5vw] lg:pr-[20vw]"
 	>
-		<div class="text-stone-300 text-lg">Hello 👋 my name is</div>
-		<h1 class="text-clampLarge text-highGreen">Kevin Jordan.</h1>
-
-		<div class="text-lg gradient-text">
-			I am a Oregon based software engineer that loves bulding digital
-			experiences. I am currently working in the telecommunications
-			industry managing an International network and a few small
-			teams. When I'm not behind the keyboard you can find me in the
-			mountains.
-		</div>
-		<button
-			class="text-highGreen self-start text-base border-highGreen border-2 p-4 rounded-md transition duration-150 ease-out hover:ease-in hover:bg-stone-300 hover:text-modernBlack"
+		<div
+			class="flex flex-col text-stone-300 text-2xl px-4 pb-[20vh] gap-[20px] md:gap-[40px] lg:max-w-[750px] lg:mx-auto"
 		>
-			Reach Out
-		</button>
-	</div>
+			<div class="text-stone-300 text-lg">Hello 👋 my name is</div>
+			<h1 class="text-clampLarge text-highGreen">Kevin Jordan.</h1>
+
+			<div class="text-lg gradient-text">
+				I am a Oregon based software engineer that loves bulding
+				digital experiences. I am currently working in the
+				telecommunications industry managing an International network
+				and a few small teams. When I'm not behind the keyboard you
+				can find me in the mountains.
+			</div>
+			<button
+				class="text-highGreen self-start text-base border-2 p-4 rounded-md transition duration-150 ease-out hover:ease-in hover:bg-stone-300 hover:text-modernBlack"
+			>
+				Reach Out
+			</button>
+		</div>
 	</section>
-	<section>
-		<h2>about me</h2>
+	<section class="flex flex-col items-start px-4">
+		<div
+			class="flex items-end text-2xl text-highGreen pb-[5vh]"
+		>
+			ABOUT<span class="block mb-[5px] ml-[10px] h-[2px] w-[200px] bg-highGreen opacity-40"></span>
+		</div>
+		<div class="text-lg gradient-text pb-4">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+			Exercitationem, illum at quaerat veniam deleniti consequuntur
+			tempora dicta aperiam expedita dolorum tempore voluptatibus
+			culpa odio in dolor, alias, ipsam velit doloribus.
+		</div>
+		<div class="text-lg gradient-text pb-4">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+			Exercitationem, illum at quaerat veniam deleniti consequuntur
+			tempora dicta aperiam expedita dolorum tempore voluptatibus
+			culpa odio in dolor, alias, ipsam velit doloribus.
+		</div>
+		<div>
+			<div class="flex flex-col gradient-text">
+				<div class="mb-8">
+					Here are the technologies that I've ben working with lately
+					:
+				</div>
+				<div>
+					<ul
+						class="grid grid-cols-2 gap-x-16 gap-y-2 p-0 list-none overflow-hidden"
+					>
+						<li><span>🤟 </span>JavaScript</li>
+						<li><span>🤟 </span>Vue 3</li>
+						<li><span>🤟 </span>Node/Express</li>
+						<li><span>🤟 </span>React</li>
+						<li><span>🤟 </span>Tailwind</li>
+						<li><span>🤟 </span>Amplify</li>
+						<li><span>🤟 </span>Postgres</li>
+						<li><span>🤟 </span>Wordpress</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</section>
 	<!-- border border-2-highGreen  -->
 </template>
 <style scoped>
 	.gradient-text {
-		background: linear-gradient(rgb(214,211,209), rgb(159, 242, 121));
+		background: linear-gradient(
+			rgb(214, 211, 209),
+			rgb(159, 242, 121)
+		);
 		background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
-	
+	/* li::before {
+		content: "▹";
+    position: absolute;
+    left: 0px;
+    color: red;
+    font-size: 16px;
+    line-height: 12px;
+	} */
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    // Example content paths...
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        deepPurp: "#61208C",
-        lightPurp: "#501D73",
-        darkGreen: "#025939",
-        springGreen: "#027333",
-        highGreen: "#9FF279",
-        darkBack : "#000212",
-        fontWhite: "#F2EFE9",
-        modernBlack: "#28282B"
-      },
-      fontFamily: {
-        oswald: ['Oswald','sans-serif'],
-        geist: ['Geist', 'sans-serif'],
-      },
-      boxShadow: {
-       meteor: "0 0 0.125em hsl(0 0% 100% / 0.3), 0 0 0.45em #9FF279"
-      },
-      fontSize: {
-        clampLarge: "clamp(40px, 8vw, 80px)"
-      },
-      backgroundImage: {
-        gradientRadial: 'radial-gradient(ellipse 80% 50% at 50% -20%,rgba(120,119,198,0.3),transparent)'
-      }
-
-    }
-  },
-  plugins: [],
-}
-
+	content: [
+		// Example content paths...
+		'./public/**/*.html',
+		'./src/**/*.{js,jsx,ts,tsx,vue}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				deepPurp: '#61208C',
+				lightPurp: '#501D73',
+				darkGreen: '#025939',
+				springGreen: '#027333',
+				highGreen: '#9FF279',
+				darkBack: '#000212',
+				fontWhite: '#F2EFE9',
+				modernBlack: '#28282B',
+			},
+			fontFamily: {
+				oswald: ['Oswald', 'sans-serif'],
+				geist: ['Geist', 'sans-serif'],
+			},
+			boxShadow: {
+				meteor:
+					'0 0 0.125em hsl(0 0% 100% / 0.3), 0 0 0.45em #9FF279',
+			},
+			fontSize: {
+				clampLarge: 'clamp(40px, 8vw, 80px)',
+			},
+			backgroundImage: {
+				gradientRadial:
+					'radial-gradient(ellipse 80% 50% at 50% -20%,rgba(120,119,198,0.3),transparent)',
+			},
+			borderCustom: {
+				guides: '1px solid #027333',
+			},
+			checkMark: {
+				content: '***',
+				position: 'absolute',
+				left: '0px',
+				color: 'red',
+				fontSize: '16px',
+				lineHeight: '12px'
+			},
+			gridTemplateColumns: {
+				skills: 'repeat(2, minmax(140px, 200px))',
+			},
+		},
+	},
+	plugins: [],
+};
