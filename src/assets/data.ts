f7733d48d367b785e617718e8aedc0fb19f32b4a
kdjordan <k.dean.jordan@gmail.com>
2023-11-12 21:25:28 -0800

    moved experience data into seperate ts file

---
+++ src/assets/data.ts
@@ -0,0 +1,51 @@
+const data = [
+  {
+    employer: 'V-tell',
+    title : 'VP Wholesale Services',
+    brief : 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    dates : '2019-Present',
+    highlights: ['Directed infrastruture selection resulting in 50% savings in monthly overhead.', 'Implemented tax reporting system that reduced staff hours by 25%', 'Coordinate projects between 3 International Teams', 'Redesigned carrier patnership program to increase active interconnects by 150%']
+  },
+  {
+    employer: 'NW Straps',
+    title : 'Owner / operator',
+    brief : 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    dates : '2015-2019',
+    highlights: ['Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ']
+  },
+  {
+    employer: '3Charm',
+    title : 'Co - Founder / Primary Developer',
+    brief : 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    dates : '2012-2015',
+    highlights: ['Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ']
+  },
+  {
+    employer: 'Tabata Project',
+    title : 'Brand Strategist / Coach',
+    brief : 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    dates : '2010-2012',
+    highlights: ['Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ']
+  },
+  {
+    employer: 'Fired Glass Company',
+    title : 'Owner / operator',
+    brief : 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    dates : '2000-2010',
+    highlights: ['Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ']
+  },
+  {
+    employer: 'Catalogfinder',
+    title : 'Lead Developer',
+    brief : 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    dates : '1998-2000',
+    highlights: ['Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ', 'Made a 03% increase in XYZ']
+  },
+  
+  
+  
+  
+  
+]
+
+export default data
--- src/components/TabbedIndex.vue
+++ src/components/TabbedIndex.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
 	import { ref } from 'vue';
+	import data from '../assets/data.ts';
 
 	const activeTab = ref<number>(0);
-	const tabs: string[] = [
-		'V-Tell',
-		'NW Straps',
-		'3Charm',
-		'Rena',
-		'CatalogFinder',
-	];
-
-	const content: string[] = [
-		'V-Tell is a web application that allows users to search for and view the details of products from a variety of online stores.',
-		'NW Straps is a web application that allows users to search for and view the details of products from a variety of online stores.',
-		'3Charm is a web application that allows users to search for and view the details of products from a variety of online stores.',
-		'Rena is a web application that allows users to search for and view the details of products from a variety of online stores.',
-		'CatalogFinder is a web application that allows users to search for and view the details of products from a variety of online stores.',
-	];
 
 	function changeTab(index: number) {
 		activeTab.value = index;
@@ -29,61 +15,58 @@
 			class="flex gap-4 overflow-x-scroll scrollbar-thin max-w-[440px] mx-auto text-[16px] font-oswald tracking-widest uppercase"
 		>
 			<button
-				v-for="(tab, index) in tabs"
+				v-for="(d, index) in data"
 				:key="index"
 				@click="changeTab(index)"
 				:class="{
 					'text-highGreen active-button': activeTab === index,
 					'text-stone-300': activeTab !== index,
-					
 				}"
-				class="cursor-pointer min-w-[100px] mb-4"
+				class="cursor-pointer min-w-[200px] mb-4 tracking-wider uppercase"
 			>
-				{{ tab }}
+				{{ d.employer }}
 			</button>
 		</div>
 		<!-- <div
 			class="w-full border-highGreen border-b-[1px] w-full overflow-visible mt-[-10px]"
 		></div> -->
-		<span class="block bg-highGreen h-[1px] opacity-40 overflow-visible"></span>
+		<span
+			class="block bg-highGreen h-[1px] opacity-40 overflow-visible"
+		></span>
 		<div class="flex flex-col items-start p-4">
-			<p
-				v-for="(desc, index) in content"
-				:key="index"
-				v-show="activeTab === index"
-				class="px-4"
-			>
-				{{ desc }}
-			</p>
+			<TransitionGroup name="list" tag="div" mode="in-out">
+				<div
+					v-for="(job, index) in data"
+					:key="index"
+					v-show="activeTab === index"
+					class="px-4"
+				>
+					<h2 class="text-stone-300 text-xl pb-2 uppercase">
+						{{ job.title }}
+					</h2>
+					<div class="text-base pl-4 pb-4">{{ job.dates }}</div>
+						<div
+							v-for="highlight in job.highlights"
+							:key="highlight"
+							class="text-stone-400 pb-4 pl-4"
+						>
+							<span class="text-highGreen">▹</span> {{ highlight }}
+						</div>
+					
+				</div>
+			</TransitionGroup>
 		</div>
 	</div>
-	<!-- <div class="flex justfy-center items-top min-h-[340px] gap-16">
-		<div
-			class="flex flex-col items-start justify-top min-w-max gap-8 pr-8"
-		>
-			<button
-				v-for="(tab, index) in tabs"
-				:key="index"
-				@click="changeTab(index)"
-				:class="{
-					'text-highGreen border-l-2 border-highGreen pl-[4px] ':
-						activeTab === index,
-					'pl-[4px] text-stone-300': activeTab !== index,
-				}"
-				class="cursor-pointer"
-			>
-				{{ tab }}
-			</button>
-		</div>
-		<div class="flex flex-col items-start">
-			<p
-				v-for="(desc, index) in content"
-				:key="index"
-				v-show="activeTab === index"
-			>
-				{{ desc }}
-			</p>
-		</div>
-	</div> -->
 </template>
 
+<style scoped>
+	.list-enter-active,
+	.list-leave-active {
+		transition: all 0.5s ease;
+	}
+	.list-enter-from,
+	.list-leave-to {
+		opacity: 0;
+		transform: translatex(10px);
+	}
+</style>
