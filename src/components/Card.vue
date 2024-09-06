e38600e13e12932ac33f044e6116bcada5f315aa
kdjordan <k.dean.jordan@gmail.com>
2024-09-06 14:56:10 -0700

    cleanup

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,96 +1,70 @@
 <template>
-	<div
-		class="flex flex-col max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-[600px] w-[300px]"
-	>
-		<a :href="link" target="_blank" rel="noopener noreferrer">
-			<img
-				class="rounded-t-lg w-full margin-auto"
-				:src="imgUrl"
-				alt=""
-			/>
-		</a>
-		<div class="p-5 flex-col h-full">
+	<div class="card">
+		<div class="cardInner flex flex-col">
 			<a :href="link" target="_blank" rel="noopener noreferrer">
-				<h5
-					class="mb-2 text-2xl tracking-loose text-gray-900 dark:text-highGreen"
-				>
-					{{ name }}
-				</h5>
+				<img class="rounded-t-lg w-full" :src="imgUrl" alt="" />
 			</a>
-			<p
-				class="mb-3 font-normal text-stone-300 max-w-full text-sizeSm"
-			>
-				{{ description }}
-			</p>
-			<p
-				v-if="notes"
-				class="text-sizeSm font-oswald text-fontWhite tracking-wider"
-			>
-				{{ `#${notes}` }}
-			</p>
-			<div
-				class="flex flex-wrap items-center gap-2 text-highGreen pt-4"
-			>
-				<span
-					v-for="(t, i) in techs"
-					:key="i"
-					class="text-sizeSm border p-1 rounded-md"
+			<div class="p-5 flex flex-col w-full flex-grow">
+				<a :href="link" target="_blank" rel="noopener noreferrer">
+					<h5
+						class="mb-2 text-2xl tracking-loose text-gray-900 dark:text-highGreen"
+					>
+						{{ name }}
+					</h5>
+				</a>
+				<p
+					class="mb-3 font-normal text-stone-300 max-w-full text-sizeSm"
 				>
-					{{ t }}
-				</span>
+					{{ description }}
+				</p>
+				<p
+					v-if="notes"
+					class="text-sizeSm font-oswald text-fontWhite tracking-wider"
+				>
+					{{ `#${notes}` }}
+				</p>
+				<div
+					class="flex flex-wrap items-center gap-2 text-highGreen pt-4"
+				>
+					<span
+						v-for="(t, i) in techs"
+						:key="i"
+						class="text-sizeSm border p-1 rounded-md"
+					>
+						{{ t }}
+					</span>
+				</div>
+				<div class="pt-2"></div>
+				<div
+					class="flex gap-4 items-center mt-auto w-full justify-between"
+				>
+					<a
+						:href="link"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-highGreen border bg-sky-900 rounded-lg hover:bg-pageBackground focus:ring-4 focus:outline-none focus:ring-blue-300 transition-colors"
+					>
+						LIVE
+					</a>
+					<a
+						v-if="github !== null"
+						class="text-highGreen"
+						:href="github"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<font-awesome-icon
+							icon="fa-brands fa-github"
+							size="2xl"
+						/>
+					</a>
+				</div>
 			</div>
-		</div>
-		<div class="flex gap-4 items-center p-4">
-			<a
-				:href="link"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-highGreen border bg-sky-900 rounded-lg hover:bg-pageBackground focus:ring-4 focus:outline-none focus:ring-blue-300 transition-colors"
-			>
-				LIVE
-				<svg
-					class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 14 10"
-				>
-					<path
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M1 5h12m0 0L9 1m4 4L9 9"
-					/>
-				</svg>
-			</a>
-			<a
-				v-if="github !== null"
-				:href="github"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="40"
-					height="40"
-					viewBox="00 0 30 24"
-					id="github"
-				>
-					<path
-						stroke="#9FF279"
-						fill="#9FF279"
-						d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z"
-					/>
-				</svg>
-			</a>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import { onMounted } from 'vue';
-
 	defineProps<{
 		imgUrl: string;
 		name: string;
@@ -100,11 +74,59 @@
 		github: string | null;
 		notes?: string;
 	}>();
-
 </script>
 
 <style scoped>
-	#github {
-		vertical-align: middle;
+	.card {
+		width: 300px;
+		height: 600px;
+		position: relative;
+		overflow: hidden;
+		display: block;
+		border-radius: 0.5rem;
+	}
+	.card .cardInner:hover {
+		/* transform: translateY(-.2rem); */
+		box-shadow: 8px 14px 31px -3px rgba(255, 255, 255, 0.3);
+	}
+	.card::before {
+		content: '';
+		display: block;
+		background: linear-gradient(
+			90deg,
+			rgba(255, 255, 255, 0) 0%,
+			rgba(191, 190, 190, 0.75) 30%,
+			rgba(255, 255, 255, 0) 100%
+		);
+		height: 100%;
+		width: 100%;
+		transform: translate(0);
+		position: absolute;
+		animation: rotate 5s linear forwards infinite;
+		z-index: 0;
+		top: 50%;
+		transform-origin: top center;
+	}
+
+	.cardInner {
+		background: #222;
+		color: #fff;
+		border-radius: 0.5rem;
+		position: relative;
+		z-index: 1;
+		height: 595px;
+		width: 295px;
+		margin: 0 auto;
+		margin-top: 2.5px;
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotate(0);
+		}
+
+		to {
+			transform: rotate(360deg);
+		}
 	}
 </style>
